38de2732f3953516905114fd14eade3b1277c8b9
Sanuth Dasanyaka <149223137+sanuth2001@users.noreply.github.com>
2024-08-17 17:53:17 +0530

    Revert "nav bar mobile responsive"

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
-import design from '../assets/SCDESIIGN.PNG'
 
 const Welcome = () => {
   return (
-    <div class= " size-8/12">
-      <img  src={design} alt="Logo" />
-      <h1 className='font-thin text-base sm:text-lg lg:text-3xl flex justify-center'>Seeking Experts in Graphics & Promotions?
-      <br/><br/>Let’s Make Your Vision Come Alive!</h1>
-     </div>
-
-     
-
+    <div>
+      <h1>Welcome to the Homepage</h1>
+    </div>
   );
 };
-""
+
 export default Welcome;
